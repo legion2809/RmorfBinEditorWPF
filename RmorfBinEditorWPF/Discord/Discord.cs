7721e00d2185621551c18e5025222575ad703b93
Shatilov Ivan <boss.vanek12@mail.ru>
2021-09-23 22:24:01 +0600

    Deleted extra dependencies from Discord.cs

--- RmorfBinEditorWPF/Discord/Discord.cs
+++ RmorfBinEditorWPF/Discord/Discord.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord;
-
-namespace Discord
+﻿namespace Discord
 {
     class Discord
     {
