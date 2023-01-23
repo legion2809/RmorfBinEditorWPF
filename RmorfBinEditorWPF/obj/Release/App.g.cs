54cfaec0900be80e3fac80f62609e08aef34d991
Yerkanat Sheriyazdanov <56153705+legion2809@users.noreply.github.com>
2023-01-23 21:51:33 +0600

    Assembly name was changed and project files were updated

--- RmorfBinEditorWPF/obj/Release/App.g.cs
+++ RmorfBinEditorWPF/obj/Release/App.g.cs
@@ -58,7 +58,7 @@ namespace RmorfBinEditorWPF {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/RmorfBinEditorWPF;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Rmorf.bin Editor;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
